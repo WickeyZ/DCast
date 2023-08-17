2e189cb68aec118c8fb4f76765403ec98c5f299f
WickeyZ <wickeychai02@gmail.com>
2023-08-17 14:37:18 +0800

    change ID

--- pages/update-voting-phase.tsx
+++ pages/update-voting-phase.tsx
@@ -194,7 +194,7 @@ export default function UpdateVotingPhasePage() {
                   type="number"
                   value={votingSessionId}
                   onChange={(e) => setVotingSessionId(parseInt(e.target.value))}
-                  id="durian-id"
+                  id="voting-session-id"
                   className="relative transition-all duration-300 py-2.5 px-4 w-full border-gray-300 dark:bg-slate-800 dark:text-white/80 dark:border-slate-600 rounded-lg tracking-wide font-light text-sm placeholder-gray-400 bg-white focus:ring disabled:opacity-40 disabled:cursor-not-allowed focus:border-blue-500 focus:ring-blue-500/20"
                   placeholder="e.g. 1"
                   required
