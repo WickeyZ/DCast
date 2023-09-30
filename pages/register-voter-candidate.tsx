df3b5cdef98c8f10e8d72a75f68c28e92d48d15f
WickeyZ <wickeychai02@gmail.com>
2023-10-01 01:28:36 +0800

    change pic logo

--- pages/register-voter-candidate.tsx
+++ pages/register-voter-candidate.tsx
@@ -349,25 +349,33 @@ export default function RegisterVoterCandidatePage() {
                               className="mx-auto mb-3 h-32 w-32 object-contain border text-gray-400"
                             />
                           ) : (
+                            // <svg
+                            //   className="mx-auto h-12 w-12 text-gray-400"
+                            //   stroke="currentColor"
+                            //   fill="none"
+                            //   viewBox="0 0 48 48"
+                            //   aria-hidden="true"
+                            // >
+                            //   <path
+                            //     strokeLinecap="round"
+                            //     strokeLinejoin="round"
+                            //     strokeWidth={2}
+                            //     d="M20 12h-4a8 8 0 00-8 8v12a8 8 0 008 8h16a8 8 0 008-8V20a8 8 0 00-8-8h-4"
+                            //   />
+                            //   <path
+                            //     strokeLinecap="round"
+                            //     strokeLinejoin="round"
+                            //     strokeWidth={2}
+                            //     d="M12 20h4l2 2 4-4 4 4 2-2h4"
+                            //   />
+                            // </svg>
                             <svg
-                              className="mx-auto h-12 w-12 text-gray-400"
-                              stroke="currentColor"
-                              fill="none"
-                              viewBox="0 0 48 48"
-                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="mx-auto mb-2 h-12 w-12 text-gray-400"
+                              viewBox="0 96 960 960"
+                              fill="currentColor"
                             >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M20 12h-4a8 8 0 00-8 8v12a8 8 0 008 8h16a8 8 0 008-8V20a8 8 0 00-8-8h-4"
-                              />
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M12 20h4l2 2 4-4 4 4 2-2h4"
-                              />
+                              <path d="M773 402v-86h-86v-60h86v-87h60v87h87v60h-87v86h-60ZM94 976q-24 0-42-18t-18-42V403q0-23 18-41.5T94 343h147l73-87h280v60H342l-73 87H94v513h680V496h60v420q0 24-18.5 42T774 976H94Zm339.5-146q72.5 0 121.5-49t49-121.5q0-72.5-49-121T433.5 490q-72.5 0-121 48.5t-48.5 121q0 72.5 48.5 121.5t121 49Zm0-60q-47.5 0-78.5-31.5t-31-79q0-47.5 31-78.5t78.5-31q47.5 0 79 31t31.5 78.5q0 47.5-31.5 79t-79 31.5Zm.5-110Z" />
                             </svg>
                           )}
                         </div>
